fdc2cb1dad33506c5c385074c3e23cb1e8269d38
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2019-01-25 17:45:05 -0600

    monkey patch base action to not work with proxied content

--- collective/library/__init__.py
+++ collective/library/__init__.py
@@ -1,4 +1,4 @@
-
+from . import patches
 from . import permissions
 
 
---
+++ collective/library/patches.py
@@ -0,0 +1,74 @@
+from Acquisition import aq_inner
+from plone.app.content.browser import contents
+from plone.app.uuid.utils import uuidToCatalogBrain
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory as _
+
+
+OriginalContentsBaseAction = contents.ContentsBaseAction
+
+
+class LibraryContentsBaseAction(OriginalContentsBaseAction):
+
+    def __call__(self, keep_selection_order=False):
+        self.protect()
+        self.errors = []
+        context = aq_inner(self.context)
+        selection = self.get_selection()
+
+        parts = str(self.request.form.get('folder', '').lstrip('/')).split('/')
+        if parts:
+            parent = self.site.unrestrictedTraverse('/'.join(parts[:-1]))
+            self.dest = parent.restrictedTraverse(parts[-1])
+
+        self.catalog = getToolByName(context, 'portal_catalog')
+        self.mtool = getToolByName(self.context, 'portal_membership')
+
+        brains = []
+        if keep_selection_order:
+            brains = [uuidToCatalogBrain(uid) for uid in selection]
+        else:
+            brains = self.catalog(UID=selection, show_inactive=True)
+
+        for brain in brains:
+            if not brain:
+                continue
+            # remove everyone so we know if we missed any
+            selection.remove(brain.UID)
+            obj = brain.getObject()
+            if (
+                self.required_obj_permission
+                and not self.mtool.checkPermission(
+                    self.required_obj_permission,
+                    obj
+                )
+            ):
+                self.errors.append(_(
+                    'Permission denied for "${title}"',
+                    mapping={'title': self.objectTitle(obj)}
+                ))
+                continue
+            obj_id = brain.getId
+            from_obj = parent
+            if parts:
+                from_obj = self.dest
+            try:
+                traversed_obj = from_obj.restrictedTraverse(obj_id)
+            except KeyError:
+                traversed_obj = None
+            if traversed_obj is not None:
+                is_content_proxy = getattr(traversed_obj, 'is_content_proxy', False)
+                is_folder_proxy = getattr(traversed_obj, 'is_folder_proxy', False)
+                if is_content_proxy or is_folder_proxy:
+                    self.errors.append(_(
+                        'Invalid operation for "${title}"',
+                        mapping={'title': self.objectTitle(traversed_obj)}
+                    ))
+                    continue
+            self.action(obj)
+
+        self.finish()
+        return self.message(selection)
+
+
+contents.ContentsBaseAction = LibraryContentsBaseAction
