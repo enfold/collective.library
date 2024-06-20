558ab900b8774f08d84d4ac83efce97db515fe8e
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2024-06-20 20:07:21 +0000

    Back to development: 1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (
     )
 
 setup(name='collective.library',
-      version="1.3",
+      version="1.4.dev0",
       description="Collective Library",
       long_description=long_description,
       # Get more strings from
