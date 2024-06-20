337f983e807efd68a35402c8e0e78daef0b0d561
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2024-06-20 14:47:40 -0500

    Fix parent_libraries field.

--- collective/library/interfaces.py
+++ collective/library/interfaces.py
@@ -1,8 +1,9 @@
 
-from collective.library.vocabularies import LibrarySource
+from .constants import LIBRARY_PORTAL_TYPE
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives as form
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
-from zope import schema
 from zope.interface import Interface
 
 
@@ -16,8 +17,16 @@ class ILibrary(ILibraryContent):
     parent_libraries = RelationList(
         title=u'Parent Libraries',
         default=list(),
-        value_type=RelationChoice(source=LibrarySource()),
+        value_type=RelationChoice(vocabulary='plone.app.vocabularies.Catalog'),
         required=False)
+    form.widget(
+        'parent_libraries',
+        RelatedItemsFieldWidget,
+        vocabulary='plone.app.vocabularies.Catalog',
+        pattern_options={
+            'selectableTypes': [LIBRARY_PORTAL_TYPE],
+        },
+    )
 
 
 class ILibraryFolder(ILibraryContent):
